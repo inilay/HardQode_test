7c6366e016fa96bee316dac7c3a02af729cd675e
inilay <indioraaa@gmail.com>
2023-09-27 19:39:38 +0300

    change user_products url

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
-from .views import ProductAPIView, AllProductsAPIView, AllUserProductsAPIView
+from .views import ProductAPIView, AllProductsAPIView, UserProductsAPIView
 
 
 urlpatterns = [
     # 1 Задание
-    path('api/v1/user_products/<int:id>/', AllUserProductsAPIView.as_view()),
+    path('api/v1/user_products/<int:id>/', UserProductsAPIView.as_view()),
     # 2 Задание
     path('api/v1/product/<int:id>/', ProductAPIView.as_view()),
     # 3 Задание
