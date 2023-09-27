27de3012359d106791f793ffc9047b1c19dc6f7a
inilay <indioraaa@gmail.com>
2023-09-27 18:23:29 +0300

    add profile permission

--- products/permissions.py
+++ products/permissions.py
@@ -9,17 +9,18 @@ class IsEnrolled(permissions.BasePermission):
             return True
 
     def has_object_permission(self, request, view, obj):
-
-        # доступ для администраторов
-        if request.user.is_superuser:
-            return True
-
-        # если пользователь владелец продукта
-        if request.user == obj.product_owner.user:
-            return True
-
-        # если записан на продукт 
         if obj in request.user.profile.enrolled_products.all():
+            return True
+
+        return False
+
+class IsProfileOwner(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return True
+    
+    def has_object_permission(self, request, view, obj):
+        if obj == request.user.profile:
             return True
 
         return False
